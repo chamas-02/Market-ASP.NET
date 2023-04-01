1445e9b1cdbf3a101851f4f8803fb1210bf6980c
Cham Bao <megabloks2010@yahoo.com>
2023-04-01 20:18:17 +0700

    Update Function
    Chức năng Login - đã có nhưng bug
    Chức năng Logout - duyệt
    Chức năng đăng ký - duyệt
    Chức năng đổi pass - vì Login bug nên này bug theo
    Trang about - duyệt
    Trang Product - tạm duyệt

--- Shopping/Shopping/Controllers/ProductController.cs
+++ Shopping/Shopping/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using PagedList.Core;
 using Shopping.Models;
 
 namespace Shopping.Controllers
@@ -13,18 +15,74 @@ namespace Shopping.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        [Route("AllProduct", Name = ("ShopProduct"))]
+        public IActionResult Index(int? page)
         {
-            return View();
+            try
+            {
+                var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+                var pageSize = 10;
+                var lsTinDangs = _context.Products
+                    .AsNoTracking()
+                    .OrderBy(x => x.DateCreated);
+                PagedList<Product> models = new PagedList<Product>(lsTinDangs, pageNumber, pageSize);
+
+                ViewBag.CurrentPage = pageNumber;
+                return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
         }
+
+        [Route("Category/{Alias}", Name = ("ListProduct"))]
+        public IActionResult List(string Alias, int page=1)
+        {
+            try
+            {
+                var pageSize = 10;
+                var danhmuc = _context.Categories.AsNoTracking().SingleOrDefault(x => x.Alias == Alias);
+
+                var lsTinDangs = _context.Products
+                    .AsNoTracking()
+                    .Where(x => x.CatId == danhmuc.CatId)
+                    .OrderByDescending(x => x.DateCreated);
+                PagedList<Product> models = new PagedList<Product>(lsTinDangs, page, pageSize);
+                ViewBag.CurrentPage = page;
+                ViewBag.CurrentCat = danhmuc;
+                return View(models);
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [Route("/{Alias}-{id}.html", Name = ("ProductDetails"))]
         public IActionResult Details(int id)
         {
-            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
-            if (product == null)
+            try
             {
-                return RedirectToAction("Index");
+                var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == id);
+                if (product == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                var lsProduct = _context.Products
+                    .AsNoTracking()
+                    .Where(x => x.CatId == product.CatId && x.ProductId != id && x.Active == true)
+                    .Take(4)
+                    .OrderByDescending(x => x.DateCreated)
+                    .ToList();
+                ViewBag.SanPham = lsProduct;
+                return View(product);
             }
-            return View(product);
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
