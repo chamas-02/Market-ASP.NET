088d56ff87d1e4a01766972006bcd9cd2476f339
Cham Bao <megabloks2010@yahoo.com>
2023-04-02 18:13:18 +0700

    amen

--- Shopping/Shopping/Models/Discount.cs
+++ Shopping/Shopping/Models/Discount.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Shopping.Models;
-
-public partial class Discount
+namespace Shopping.Models
 {
-    public int DiscountId { get; set; }
+    public partial class Discount
+    {
+        public Discount()
+        {
+            ProductDiscounts = new HashSet<ProductDiscount>();
+        }
 
-    public double ReducePercent { get; set; }
+        public int DiscountId { get; set; }
+        public double ReducePercent { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public byte Active { get; set; }
+        public int CreatorId { get; set; }
+        public DateTime CreatedDate { get; set; }
 
-    public DateTime StartTime { get; set; }
-
-    public DateTime EndTime { get; set; }
-
-    public byte Active { get; set; }
-
-    public int CreatorId { get; set; }
-
-    public DateTime CreatedDate { get; set; }
-
-    public virtual Account Creator { get; set; } = null!;
-
-    public virtual ICollection<ProductDiscount> ProductDiscounts { get; } = new List<ProductDiscount>();
+        public virtual Account Creator { get; set; } = null!;
+        public virtual ICollection<ProductDiscount> ProductDiscounts { get; set; }
+    }
 }
